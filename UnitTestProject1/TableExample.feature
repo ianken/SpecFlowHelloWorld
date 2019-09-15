f45c5cf20539f0fb76c19f60d059025436e486c9
Ian Kennedy <iank@outlook.com>
2019-09-14 18:06:35 -0700

    Added sample on using a simple table for validating multple data sets.

---
+++ UnitTestProject1/TableExample.feature
@@ -0,0 +1,16 @@
+﻿Feature: TableExample
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add numbers from table
+	Given user enters the follwing values
+	| FirstNumber | SecondNumber |
+	| 15		  | 10		     |
+	| 23		  | 10           |
+	Then results should be validted
+	| result |
+	| 25  |
+	| 33  |
+
---
+++ UnitTestProject1/TableExampleSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Calc;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+
+
+    [Binding]
+    public class TableExampleSteps
+    {
+        List<int> results = new List<int>();
+        IEnumerable<Calculator> cal;
+
+        [Given(@"user enters the follwing values")]
+        public void GivenUserEntersTheFollwingValues(Table table)
+        {
+            cal = table.CreateSet<Calculator>();
+
+            //Run all the calculations and add them to the results list
+            foreach (Calculator c in cal)
+            {
+                results.Add(c.Add());
+            }
+
+        }
+        
+        [Then(@"results should be validted")]
+        public void ThenResultsShouldBeValidted(Table table)
+        {
+            int index = 0;
+
+            if (table.RowCount > results.Count)
+                throw new IndexOutOfRangeException("Input data and output data row counts do not match...");
+
+            foreach (TableRow r in table.Rows)
+            {
+                //There's only one column, so this will work. 
+                //TableRow has no indexer...
+                foreach (var res in r.Values)
+                {
+                    Assert.AreEqual(Convert.ToInt32(res), results[index]);
+                    index++;
+                }
+            }
+
+
+        }
+    }
+}
