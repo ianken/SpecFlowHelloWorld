00c8d84de84e2fc0a8bcf882761aed94f45b9431
Ian Kennedy <iank@outlook.com>
2019-09-25 11:22:07 -0700

    Added simple Selenium Web Driver example to SpecFlow sample.

---
+++ UnitTestProject1/SeleniumSample.feature
@@ -0,0 +1,8 @@
+﻿Feature: SeleniumSample
+	Selenium web driver hello world
+
+@mytag
+Scenario: Simple Selenium Sample
+	Given Chrome is running
+	And I have navigated to https://www.bing.com/
+	Then verify website destiantion https://www.bing.com/
---
+++ UnitTestProject1/SeleniumSampleSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class SeleniumSampleSteps
+    {
+        IWebDriver driver;
+
+        [Given(@"Chrome is running")]
+        public void GivenChromeIsRunning()
+        {
+            driver = new ChromeDriver(@"C:\");
+        }
+        
+        [Given(@"I have navigated to (.*)")]
+        public void GivenIHaveNavigatedToBing_Com(string target)
+        {
+            driver.Navigate().GoToUrl(target);
+        }
+        
+        [Then(@"verify website destiantion (.*)")]
+        public void ThenVerifyWebsiteLoaded(string target)
+        {
+            //This doesn't actually verify anything...
+            Assert.AreEqual(driver.Url, target);
+            driver.Quit();
+        }
+    }
+}
